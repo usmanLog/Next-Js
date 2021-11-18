ed27f62fee076d104a190b8d21f513077c1c07fb
Usman Rasheed <usmaanfatal@gmail.com>
2021-11-18 10:01:52 +0500

    Add API Routes and getStaticProps

---
+++ dj-events/dj-events-frontend/components/EventItem.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/EventItem.module.css";
+
+export default function EventItem({ event }) {
+  return (
+    <div className={styles.event}>
+      <div className={styles.img}>
+        <Image
+          src={event.image ? event.image : "/images/event.default.png"}
+          alt="hehe"
+          height={100}
+          width={170}
+        />
+      </div>
+
+      <div className={styles.info}>
+        <span>
+          {event.date} at {event.time}
+        </span>
+        <h3>{event.name}</h3>
+      </div>
+
+      <div className={styles.link}>
+        <Link href={`/events/${event.slug}`}>
+          <a className="btn">Detauks</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- dj-events/dj-events-frontend/pages/index.js
+++ dj-events/dj-events-frontend/pages/index.js
@@ -1,9 +1,34 @@
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
+import { API_URL } from "../config";
+import EventItem from "../components/EventItem";
+import Link from "next/link";
 
-export default function Home() {
-   return (
-      <Layout>
-         <h1>Home</h1>
-      </Layout>
-   );
+export default function HomePage({ events }) {
+  console.log(events);
+
+  return (
+    <Layout>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} event={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
 }
