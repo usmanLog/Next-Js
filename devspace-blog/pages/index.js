04aed96da09cd331acec79a157cb7dec8953eecf
Usman Rasheed <usmaanfatal@gmail.com>
2021-11-29 11:45:01 +0500

    Get Posts & Parse Frontmatter

--- devspace-blog/pages/index.js
+++ devspace-blog/pages/index.js
@@ -1,3 +1,37 @@
-export default function Home() {
-  return <div></div>;
+import fs from "fs";
+import path from "path";
+import Layout from "../components/Layout";
+import matter from "gray-matter";
+
+export default function Home({ posts }) {
+  return (
+    <Layout>
+      <h1>Hello World</h1>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(path.join("posts", filename), "utf-8");
+
+    const { data: frontMatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontMatter,
+    };
+  });
+
+  console.log(posts);
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
