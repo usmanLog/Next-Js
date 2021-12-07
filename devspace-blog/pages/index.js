404205b7894df1f029c7daedc686b3f81da3e4c3
Usman Rasheed <usmaanfatal@gmail.com>
2021-12-07 08:39:01 +0500

    Display Page Posts

--- devspace-blog/pages/index.js
+++ devspace-blog/pages/index.js
@@ -2,11 +2,26 @@ import fs from "fs";
 import path from "path";
 import Layout from "../components/Layout";
 import matter from "gray-matter";
+import Link from "next/link";
+import Post from "../components/Post";
+import { sortByDate } from "../utils";
 
 export default function Home({ posts }) {
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
 }
@@ -21,17 +36,16 @@ export async function getStaticProps() {
 
     const { data: frontMatter } = matter(markdownWithMeta);
 
+    console.log(frontMatter);
     return {
       slug,
       frontMatter,
     };
   });
 
-  console.log(posts);
-
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate).slice(0, 6),
     },
   };
 }
